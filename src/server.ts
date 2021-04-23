b224c2ae0e156031feece719ce62fa47cb393ceb
cleuJunior <cleu.junior@gmail.com>
2021-04-23 00:13:17 -0300

    2º Aula Node NextLevelWe #4

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
+import "./database";
+import { router } from './routes';
 
 const app = express();
 
-/*
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Altereçao específica
-*/
-
-// http;//localhost:3333/users
-app.get("/", (request, response) => {
-    return response.json({"menssage": 'Hello World - NLW'});
-});
-
-// 1 param => Rota(Recurso API)
-// 2 param => requeste, reponse
-
-app.post("/", (request, response) => {
-    return response.json({'message': 'Dados Salvo com sucesso'});
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () =>
     console.log('Servidor funcionando!')
